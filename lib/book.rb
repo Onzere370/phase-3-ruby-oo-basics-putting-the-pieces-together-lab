752931ae7cdb9daf17ef31e41e60a612c0b1d48a
cynthia <cynthiakagz@gmail.com>
2023-07-01 13:25:17 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+    def initialize (title)
+        @title= title
+        @author= ""
+        @page_count=""
+        @genre= ""
+        @turn_page= ""
+    end
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
